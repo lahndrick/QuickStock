d3ab99d19c0553754ea1025d2c5381e7344ebc9f
Alex <alex@netspacenz.com>
2023-11-13 20:39:04 +1300

    sidebar

---
+++ application/views/sidebar.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" href="<?php echo base_url('application/assets/css/styles.css'); ?>">
+<div id="sidebar">
+	<ul>
+		<li><a>First</a></li>
+		<li><a>Second</a></li>
+		<li><a>Third</a></li>
+	</ul>
+</div>
