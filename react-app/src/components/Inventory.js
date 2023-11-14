b2c2bd6514f2c5197f6b014ac30e607825477b7d
lahndrick <lahndrickhendricks2@gmail.com>
2023-11-14 20:16:33 +1300

    test

--- react-app/src/components/Inventory.js
+++ react-app/src/components/Inventory.js
@@ -1,19 +1,31 @@
-import React, {useEffect} from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 function Inventory() {
-	useEffect(() => {
-		// Set the page title when the component mounts
-		document.title = 'Inventory';
+	const [invData, setInvData] = useState([]);
 
-		// Optionally, you can reset the title when the component unmounts
-		return () => {
-			document.title = 'QuickSort'; // Replace with your original title
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await axios.get("http://3.26.71.160/index.php/main/getDashboardData");
+				setInvData(response.data);
+			} catch (error) {
+				console.error('Error fetching data:', error.message);
+			}
 		};
+
+		fetchData();
 	}, []);
+
 	return (
-		<section className="dashboard-section">
-			{/* Dashboard content goes here */}
+		<section className="inventory-section">
+			{/* ... */}
 			<h2>Inventory</h2>
+			<ul>
+				{invData.map(item => (
+					<li key={item.id}>{item.name}</li>
+				))}
+			</ul>
 			{/* ... */}
 		</section>
 	);
