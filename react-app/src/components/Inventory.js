07efaf08556c7c8a52122b1f77e11052231bff0b
lahndrick <lahndrickhendricks2@gmail.com>
2023-11-19 14:35:14 +1300

    fix inventory items shown

--- react-app/src/components/Inventory.js
+++ react-app/src/components/Inventory.js
@@ -14,6 +14,7 @@ function Inventory() {
 					},
 				});
 				console.log('Response Data:', response.data);
+				setInvData(response.data);
 			} catch (error) {
 				console.error('Error fetching data:', error.message);
 			}
