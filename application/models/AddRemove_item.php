c005d4344a02e6cd1229bf8858d8235518b278ef
lahndrick <lahndrickhendricks2@gmail.com>
2023-11-19 22:45:35 +1300

    create methods add/remove

---
+++ application/models/AddRemove_item.php
@@ -0,0 +1,24 @@
+<?php
+class AddRemove_items extends CI_model
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->database();
+    }
+
+    public function addItem($data)
+    {
+        $this->db->insert("dashboard", $data);
+
+        return $this->db->affected_rows() != 0;
+    }
+
+    public function removeItem($item_name)
+    {
+        $this->db->where('name', $item_name);
+        $this->db->delete('dashboard');
+
+        return $this->db->affected_rows() != 0;
+    }
+}
